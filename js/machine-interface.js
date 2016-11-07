58bd3922347a1409a0540066d0bfa67a32b40992
Blake Scala and Caleb Stevenson <blake+caleb@>
2016-11-07 12:05:34 -0800

    add machine back and interface logic for loop toggle

--- js/machine-interface.js
+++ js/machine-interface.js
@@ -1,1 +1,7 @@
 var machine = require('./../js/machine.js').MachineModule;
+
+$(function() {
+  $("#toggle").click(function() {
+    machine.toggleLoop();
+  });
+});
--- js/machine.js
+++ js/machine.js
@@ -1,1 +1,27 @@
 exports.MachineModule = Machine;
+
+var steps = 16;
+var i = 0;
+var playing = false;
+
+Machine.prototype.toggleLoop = function() {
+  var loop;
+  if (!this.playing) {
+    loop = setInterval(integerPrint, 125);
+    this.playing = true;
+  } else {
+    clearInterval(loop);
+    this.playing = false;
+  }
+};
+
+Machine.prototype.integerPrint = function() {
+  if( i < steps) {
+    i++;
+    console.log(i);
+  } else {
+    i = 0;
+    i++;
+    console.log(i);
+  }
+};
