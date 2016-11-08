909cba1f1a25bd9ba1839e57ade0e29a5a11bd7c
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-08 10:17:12 -0800

    write function to generate audio tags and data storage, change where i was incremented in machine class to fix console log bug

--- js/machine-interface.js
+++ js/machine-interface.js
@@ -1,15 +1,42 @@
 var Machine = require('./../js/machine.js').MachineModule;
 var machine = new Machine();
 
+var allSounds = [];
+
+
+function createSounds(_sound){
+  var soundArray = [];
+  for (var i = 0; i < 16; i++) {
+    $("#sounds").append("<audio src='public/sounds/" + _sound + ".WAV' id='" + _sound + i + "' controls></audio>");
+    var sound = document.getElementById(_sound + i);
+    soundArray.push(sound);
+  }
+  allSounds.push(soundArray);
+  console.log(allSounds);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 $(function() {
   for (var i = 1; i <= machine.steps; i++) {
     $("#steps").append('<div class="step-unselected" id="col' + i + 'row1"></div>');
   }
   $("#toggle").click(function() {
-    if (machine.playing) {
-      machine.stopLoop();
-    } else {
-      machine.toggleLoop();
-    }
+    createSounds("bass1");
+    //
+    // if (machine.playing) {
+    //   machine.stopLoop();
+    // } else {
+    //   machine.toggleLoop();
+    // }
   });
 });
--- js/machine.js
+++ js/machine.js
@@ -10,9 +10,38 @@ Machine.prototype.toggleLoop = function() {
   var _this = this;
   function metronome() {
     if( _this.i < _this.steps) {
-      _this.i++;
+      console.log(_this.i);
       console.log("If: " + _this.boolArray[_this.i]);
+
+      if (_this.i%8 === 0) {
+        var bass8 = document.getElementById("bass8");
+        bass8.play();
+      } else if(_this.i%7 === 0){
+        var bass7 = document.getElementById("bass7");
+        bass7.play();
+      } else if(_this.i%6 === 0) {
+        var bass6 = document.getElementById("bass6");
+        bass6.play();
+      } else if(_this.i%5 === 0){
+        var bass5 = document.getElementById("bass5");
+        bass5.play();
+      } else if(_this.i%4 === 0) {
+        var bass4 = document.getElementById("bass4");
+        bass4.play();
+      } else if(_this.i%3 === 0) {
+        var bass3 = document.getElementById("bass3");
+        bass3.play();
+      } else if(_this.i%2 === 0) {
+        var bass2 = document.getElementById("bass2");
+        bass2.play();
+      } else{
+        var bass1 = document.getElementById("bass1");
+        bass1.play();
+      }
+      _this.i++;
     } else {
+      var bass1 = document.getElementById("cymbal1");
+      bass1.play();
       _this.i = 0;
       _this.i++;
       console.log("Else " + _this.boolArray[_this.i]);
