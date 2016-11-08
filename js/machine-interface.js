3ad318bdb5c3d3f339ba7467532da59d93430510
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-08 10:49:24 -0800

    refactor audio generation as machine method and play all sounds through metronome

--- js/machine-interface.js
+++ js/machine-interface.js
@@ -1,42 +1,17 @@
 var Machine = require('./../js/machine.js').MachineModule;
 var machine = new Machine();
 
-var allSounds = [];
-
-
-function createSounds(_sound){
-  var soundArray = [];
-  for (var i = 0; i < 16; i++) {
-    $("#sounds").append("<audio src='public/sounds/" + _sound + ".WAV' id='" + _sound + i + "' controls></audio>");
-    var sound = document.getElementById(_sound + i);
-    soundArray.push(sound);
-  }
-  allSounds.push(soundArray);
-  console.log(allSounds);
-}
-
-
-
-
-
-
-
-
-
-
-
-
 $(function() {
+  machine.createSounds("bass1");
+  machine.createSounds("cymbal1");
   for (var i = 1; i <= machine.steps; i++) {
     $("#steps").append('<div class="step-unselected" id="col' + i + 'row1"></div>');
   }
   $("#toggle").click(function() {
-    createSounds("bass1");
-    //
-    // if (machine.playing) {
-    //   machine.stopLoop();
-    // } else {
-    //   machine.toggleLoop();
-    // }
+    if (machine.playing) {
+      machine.stopLoop();
+    } else {
+      machine.toggleLoop();
+    }
   });
 });
